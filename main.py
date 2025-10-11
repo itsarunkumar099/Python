7ce519eb3838bb27347e1c5366923d9676529d8e
itsarunkumar099 <arunsain063@gmail.com>
2025-10-12 01:27:23 +0530

    Update

--- main.py
+++ main.py
@@ -952,3 +952,8 @@ text = f.read()
 print(text)
 f.close()
 
+# OR
+
+with open('myfile.txt', 'r') as f:  # Using 'with' statement to automatically close the file
+    text = f.read()  # Reading the entire file content
+    print(text)  # Printing the content of the file
